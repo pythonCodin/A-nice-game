39c311e0b62a7e4eac81032b4cf99536956b39f1
pythonCodin <79765126+pythonCodin@users.noreply.github.com>
2021-06-25 07:00:55 -0700

    Add files via upload

---
+++ game.py
@@ -0,0 +1,66 @@
+import turtle
+import time
+wn = turtle.Screen()
+wn.title('Boring game')
+wn.bgcolor('black')
+wn.setup(width=600, height=600)
+wn.tracer(0)
+ball = turtle.Turtle()
+start_game = turtle.Turtle()
+start_game.speed(0)
+start_game.color('white')
+start_game.penup()
+start_game.hideturtle()
+start_game.goto(0,0)
+start_game.write('Press s to start and q to quit', align='center', font=('Courier', '30', 'bold'))
+x = 1
+def game():
+    start_game.clear()
+    ball.speed(0)
+    ball.shape('square')
+    ball.color('white')
+    ball.penup()
+    ball.goto(0, 0)
+def up():
+    ball.setheading(90)
+def down():
+    ball.setheading(270)
+def left():
+    ball.setheading(180)
+def right():
+    ball.setheading(0)
+def quit_game():
+    wn.bye()
+wn.listen()
+wn.onkey(game, 's')
+wn.onkey(up, 'Up')
+wn.onkey(down, 'Down')
+wn.onkey(left, 'Left')
+wn.onkey(right, 'Right')
+wn.onkey(quit_game, 'q')
+
+while True:
+    wn.update()
+
+    ball.forward(x)
+    
+    if ball.ycor() > 278:
+        ball.goto(0, 0)
+        time.sleep(1)
+        x *= 2
+
+    if ball.ycor() < -278:
+        ball.goto(0, 0)
+        time.sleep(1)
+        x *= 2
+
+    if ball.xcor() > 278:
+        ball.goto(0, 0)
+        time.sleep(1)
+        x *= 2
+
+
+    if ball.xcor() < -278:
+        ball.goto(0, 0)
+        time.sleep(1)
+        x *= 2
